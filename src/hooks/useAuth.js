ac465b930fc114ba3ff539b3125cf72e626b69f5
Brandon A <byarrieta@protonmail.com>
2022-02-24 23:24:32 -0500

    agregar token a cokie

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,6 +1,6 @@
 import React, {useState, useContext, createContext} from "react";
 import Cookie from 'js-cookie'
-import axios  from "axios";
+import axios from "axios";
 
 
 import endPoints from '../services/api'
@@ -31,13 +31,27 @@ function useProviderAuth() {
 
     const {data: access_token} = await axios.post(endPoints.auth.login, {email, password}, options)
 
-    if(access_token){
-      Cookie.set('token', access_token.access_token, {expires: 5})
+    if (access_token) {
+      const token = access_token.access_token
+
+      Cookie.set('token', token, {expires: 5})
+
+      axios.defaults.headers.Authorization = `Bearer ${token}`;
+      const {data: user} = await axios.get(endPoints.auth.profile)
+      console.log(user)
+      setUser({
+        "name": "Nicolas",
+        "email": "nico@gmail.com",
+        "password": "123",
+        "id": 4,
+        "role": "customer"
+      },)
+
     }
 
   }
 
-  return{
+  return {
     user,
     signIn
   }
